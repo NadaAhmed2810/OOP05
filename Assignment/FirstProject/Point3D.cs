2792cb4202e2e2fa4a429b1d97482148c398d9cf
NadaAhmed2810 <nwrabsywny534@gmail.com>
2025-01-13 00:13:55 +0200

    Q2 And Q3

---
+++ Assignment/FirstProject/Point3D.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment.FirstProject
+{
+    internal class Point3D
+    {
+        #region Property
+        public double X { get; set; }
+        public double Y { get; set; }
+        public double Z { get; set; }
+        #endregion
+
+        #region Constructors
+        public Point3D(double x, double y, double z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+
+        public Point3D(double x):this(x,x,x)
+        {
+           
+        }
+
+        public Point3D()
+        {
+        }
+        #endregion
+        #region Methods
+        public override string ToString()
+        {
+            return $"Point Coordinates: ({X},{Y},{Z})";
+        }
+        #endregion
+
+
+
+    }
+}
---
+++ Assignment/Program.cs
@@ -0,0 +1,44 @@
+﻿using Assignment.FirstProject;
+
+namespace Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region First Project
+            #region Q2:
+            //Point3D P = new Point3D(10, 10, 10);
+            //Console.WriteLine(P.ToString());
+            #endregion
+            #region Q3: Read from the User the Coordinates for 2 points P1, P2 (Check the input using try Pares, Parse, Convert).
+            double x=0, y=0, z=0;
+            bool f = false;
+
+            Console.WriteLine($"Enter coordinates for (X, Y, Z):");
+            do
+            {
+                Console.Write("X: ");
+                f = double.TryParse(Console.ReadLine(), out x);
+            } while (!f);
+            f = false;
+            do
+            {
+                Console.Write("Y: ");
+                f = double.TryParse(Console.ReadLine(), out y);
+            } while (!f);
+
+            f = false;
+            do
+            {
+                Console.Write("Z: ");
+                f = double.TryParse(Console.ReadLine(), out z);
+            } while (!f);
+            Point3D point3D= new Point3D(x, y, z);
+            Console.WriteLine(point3D.ToString());
+            #endregion
+
+            #endregion
+        }
+    }
+}
